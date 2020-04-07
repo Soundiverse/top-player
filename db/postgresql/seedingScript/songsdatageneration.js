a79967713c80d5258829332953b3c64a597c1ed6
Phuc Tran <phuctranlv@gmail.com>
2020-04-07 11:36:45 -0700

    Update the data generation scripts to reflect new schema

--- db/postgresql/seedingScript/songsdatageneration.js
+++ db/postgresql/seedingScript/songsdatageneration.js
@@ -7,13 +7,15 @@ const imageUrl = 'https://soundiverse.s3-us-west-1.amazonaws.com/';
 
 const songsTableDataWriteStream = fs.createWriteStream('songstable.csv');
 
-const numberOfPrimaryRecords = 100000;
+const numberOfPrimaryRecords = 1000000;
+const maxNumberOfSecondaryRecords = 100;
 
 console.log(`Creating ${numberOfPrimaryRecords} records of songs`);
 
 function writeAlot(writer, encoding, callback) {
   let i = numberOfPrimaryRecords;
   let j = 0;
+  let userId = faker.random.number(numberOfPrimaryRecords) + 1;
   function write() {
     let ok = true;
     do {
@@ -24,16 +26,16 @@ function writeAlot(writer, encoding, callback) {
       j += 1;
       const songId = j;
       const songName = faker.random.word('string').replace(',', '');
-      const userId = faker.random.number(1000000);
-      const playListId = faker.random.number(1000000);
-      const length = faker.random.number(300);
-      const timeCreatedAt = faker.date.past(20);
-      const tag = faker.random.word('string').replace(',', '');
+      if (Math.ceil(Math.random() * faker.random.number(maxNumberOfSecondaryRecords)) === 1) {
+        userId = faker.random.number(numberOfPrimaryRecords) + 1;
+      }
+      const timeSongCreatedAt = faker.date.past(20);
       const songCover = `${imageUrl}${faker.random.number(9)}.jpeg`;
       const soundwaveImage = faker.random.word('string').replace(',', '');
       const songFile = `${songUrl}${faker.random.number(6)}.mp3`;
-      const commentId = faker.random.number(1000000);
-      const data = `${songId},${songName},${userId},${playListId},${length},${timeCreatedAt},${tag},${songCover},${soundwaveImage},${songFile},${commentId}\n`;
+      const playlist = faker.random.word('string').replace(',', '');
+      const tag = faker.random.word('string').replace(',', '');
+      const data = `${songId},${songName},${userId},${timeSongCreatedAt},${songCover},${soundwaveImage},${songFile},${playlist},${tag}\n`;
       if (i === 0) {
         writer.write(data, encoding, callback);
       } else {
