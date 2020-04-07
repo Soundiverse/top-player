b4c7b4a16fef2cc4902da154bc09eecd87e4b8ea
Phuc Tran <phuctranlv@gmail.com>
2020-04-07 11:29:27 -0700

    Remove redundancy in schema
    Remove redundancy that force erronous duplication of the unique column
    of the tables.
    
    Remove tags table since there is enough complexity in the schema.
    
    Remove playlist columns in all table since that does not exist in
    the actual module.

--- cassandraschema.sql
+++ cassandraschema.sql
@@ -1,7 +1,7 @@
 DROP KEYSPACE IF EXISTS topplayerservice;
 
 CREATE KEYSPACE IF NOT EXISTS topplayerservice
-  WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 3};
+  WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};
 
 USE topplayerservice;
 
@@ -11,37 +11,23 @@ CREATE TABLE songs (
   userid int,
   username varchar,
   useravatar varchar,
-  playlistid int,
-  playlistname varchar,
-  length int,
   timesongcreatedat varchar,
   tag varchar,
   songcover varchar,
   soundwaveimage varchar,
   songfile varchar,
-  commentid int,
-  comment varchar,
-  timeonsong int,
-  timecommentcreatedat varchar,
-  replyid int,
-  reply varchar,
-  timereplycreatedat varchar,
-  PRIMARY KEY (userid, timesongcreatedat)
-) WITH CLUSTERING ORDER BY (timesongcreatedat DESC);
+  PRIMARY KEY (songid, userid)
+);
 
 CREATE TABLE users (
   userid int,
   username varchar,
   useravatar varchar,
-  songid int,
-  songname varchar,
-  playlistid int,
-  playlistname varchar,
   location varchar,
   followers int,
   following int,
   personallink varchar,
-  PRIMARY KEY (location)
+  PRIMARY KEY (userid, location)
 );
 
 CREATE TABLE comments (
@@ -54,24 +40,11 @@ CREATE TABLE comments (
   songname varchar,
   timeonsong int,
   timecommentcreatedat varchar,
-  replyid int,
-  reply varchar,
-  PRIMARY KEY (songid, timecommentcreatedat)
-) WITH CLUSTERING ORDER BY (timecommentcreatedat DESC);
-
-CREATE TABLE tags (
-  tag varchar,
-  songid int,
-  songname varchar,
-  playlistid int,
-  playlistname varchar,
-  PRIMARY KEY (playlistid)
+  PRIMARY KEY (songid, songname, timeonsong, commentid)
 );
 
-COPY tags (tag, songid, songname, playlistid, playlistname) FROM '/Users/phuctran/Documents/bootcamp/Soundiverse/top-player-service/db/cassandra/data/tagstable.csv';
+COPY songs (songid,songname,userid,username,useravatar,timesongcreatedat,tag,songcover,soundwaveimage,songfile) FROM '/Users/phuctran/Documents/bootcamp/Soundiverse/top-player-service/db/cassandra/data/songstable.csv';
 
-COPY songs (songid,songname,userid,username,useravatar,playlistid,playlistname,length,timesongcreatedat,tag,songcover,soundwaveimage,songfile,commentid,comment,timeonsong,timecommentcreatedat,replyid,reply,timereplycreatedat) FROM '/Users/phuctran/Documents/bootcamp/Soundiverse/top-player-service/db/cassandra/data/songstable.csv';
+COPY users (userid,username,useravatar,location,followers,following,personallink) FROM '/Users/phuctran/Documents/bootcamp/Soundiverse/top-player-service/db/cassandra/data/userstable.csv';
 
-COPY users (userId,userName,userAvatar,songId,songName,playListId,playListName,location,followers,following,personalLink) FROM '/Users/phuctran/Documents/bootcamp/Soundiverse/top-player-service/db/cassandra/data/userstable.csv';
-
-COPY comments (commentId,comment,userId,userName,userAvatar,songId,songName,timeOnSong,timeCommentCreatedAt,replyId,reply) FROM '/Users/phuctran/Documents/bootcamp/Soundiverse/top-player-service/db/cassandra/data/commentstable.csv';
+COPY comments (commentid,comment,userid,userName,useravatar,songid,songname,timeonsong,timecommentcreatedat) FROM '/Users/phuctran/Documents/bootcamp/Soundiverse/top-player-service/db/cassandra/data/commentstable.csv';
