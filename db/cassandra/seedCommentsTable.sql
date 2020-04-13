050fd68673dc5a9e150bdeaa0af83b66fc246cdf
Phuc Tran <phuctranlv@gmail.com>
2020-04-12 21:47:33 -0700

    Correct path for COPY statement

--- db/cassandra/seedCommentsTable.sql
+++ db/cassandra/seedCommentsTable.sql
@@ -1,3 +1,3 @@
 USE topplayerservice;
 
-COPY comments (commentid,comment,userid,username,avatar,songid,songtitle,timeonsong,timecommentcreatedat) FROM '/Users/phuctran/Documents/bootcamp/Soundiverse/top-player-service/db/cassandra/data/commentstable.csv';
+COPY comments (commentid,comment,userid,username,avatar,songid,songtitle,timeonsong,timecommentcreatedat) FROM '/db/cassandra/data/commentstable.csv';
--- db/cassandra/seedUsersTable.sql
+++ db/cassandra/seedUsersTable.sql
@@ -1,3 +1,3 @@
 USE topplayerservice;
 
-COPY users (userid,username,avatar,location,followers,following,personallink) FROM '/Users/phuctran/Documents/bootcamp/Soundiverse/top-player-service/db/cassandra/data/userstable.csv';
+COPY users (userid,username,avatar,location,followers,following,personallink) FROM '/db/cassandra/data/userstable.csv';
